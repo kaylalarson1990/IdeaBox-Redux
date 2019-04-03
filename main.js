49ad4bce3a0c590cbddb52acc3e7bae246da7e42
Kayla Larson <kaylalarson@MacBook-Pro-2.local>
2019-04-03 15:42:58 -0600

    Append idea card in JS
    Co-Authored-by: Edward <edwardjackc@gmail.com>
    Co-Authored-by: Taylor <taylornoel418@icloud.com>

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ var bodyInput = document.querySelector("#body-input");
 var saveBtn = document.querySelector("#btn-save");
 var searchBtn = document.querySelector("#btn-search");
 var searchInput = document.querySelector("#search-input");
-// var ideaCard = document.querySelector("#idea-card");
+var ideaCard = document.querySelector("#idea-card");
 var starIcon = document.querySelector("#star-icon");
 var closeIcon = document.querySelector("#close-icon");
 var upvoteIcon = document.querySelector("#upvote-icon");
@@ -20,8 +20,7 @@ var cardPara = document.querySelector("#card-paragraph");
 var qualityType = document.querySelector("#quality-type");
 
 var ideaContainer = document.querySelector(".bottom-section")
-var ideaCard = document.createElement("div");
-ideaCard.classList.add("idea-card")
+
 
 var ideas = [];
 
@@ -43,27 +42,35 @@ saveBtn.addEventListener("click", saveInput)
 titleInput.addEventListener("keyup", enableBtn)
 
 
+
+
+
 /*---------------- Functions ------------*/
 
-function saveInput() {
-	var newTitle = titleInput.value;
-	console.log(newTitle);
-	var newBody = bodyInput.value;
-	console.log(newBody);
-	ideaCard.innerHTML = 
-			`<header class="idea-card-header">
-				<img src="images/star.svg" class="idea-card-icons" id="star-icon"/>
-				<img src="images/delete.svg" class="idea-card-icons" id="close-icon"/>
-			</header>
-        <h2 id="card-title">${newTitle}</h2>
-        <p class="idea-card-paragraph" id="card-paragraph">${newBody}</p>
+function createNewIdea() {
+  var newTitle = titleInput.value;
+  console.log(newTitle);
+  var newBody = bodyInput.value;
+  console.log(newBody);
+  ideaContainer.innerHTML += 
+      `<figure class="idea-card" id="idea-card" contenteditable = "true"><header class="idea-card-header">
+        <img src="images/star.svg" class="idea-card-icons" id="star-icon"/>
+        <img src="images/delete.svg" class="idea-card-icons" id="close-icon"/>
+      </header>
+        <h2 id="card-title" contenteditable = "true">${newTitle}</h2>
+        <p class="idea-card-paragraph" id="card-paragraph" contenteditable = "true">${newBody}</p>
       <div class="idea-card-footer">
           <img src="images/upvote.svg" class="upvote-icon idea-card-icons" id="upvote-icon"/>
           <p>Quality:<span class="quality" id="quality-type">Swill</span></p>
           <img src="images/downvote.svg" class="downvote-icon idea-card-icons" id="downvote-icon"/>
-      </div>
+      </div></figure>
       `
-  ideaContainer.append(ideaCard);
+
+
+}
+
+function saveInput() {
+	createNewIdea();
   storeInput();
   clearInputs();
 }
@@ -74,9 +81,7 @@ function storeInput(id, title, body) {
 	localStorage.setItem(newIdea.id, stringified);
 }
 
-// function retrieveInput()
-// var retrieved =localStorage.getItem(stringified);
-// var parsed = JSON.parse(retrieved);
+
 
 
 function clearInputs() {
